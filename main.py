0b8602ba04b54b97fa6db6d9e5013f9d9b1d0982
manonmission88 <manishnewray88@gmail.com>
2025-06-13 12:43:36 -0400

    load function

---
+++ main.py
@@ -0,0 +1,26 @@
+import os 
+import sys 
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("Error: Please provide a prompt as a command line argument.")
+        sys.exit(1)
+
+    prompt = "".join(sys.argv[1:])
+
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents= prompt 
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
