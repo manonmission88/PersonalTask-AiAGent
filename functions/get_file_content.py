005aab0c85f352e428cde1c0b3636f1539208a9d
manonmission88 <manishnewray88@gmail.com>
2025-06-18 00:28:53 -0400

    get file content

---
+++ functions/get_file_content.py
@@ -0,0 +1,31 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    try:
+        # Resolve absolute paths
+        working_directory = os.path.abspath(working_directory)
+        file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+        # Check if the file is within the working directory
+        if not file_path.startswith(working_directory):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        # Check if the file exists and is a file
+        if not os.path.isfile(file_path):
+            return f"Error: File not found or is not a regular file: {file_path}"
+
+        # Read and return the file content
+        #truncate the content 
+        max_chars = 10000
+        with open(file_path, 'r') as file:
+            content = file.read()
+            # print(len(content),"+++")
+            if len(content) > max_chars:
+                truncated_content = content[:max_chars] + f"\n[...File \"{file_path}\" truncated at 10000 characters]"
+                return truncated_content
+            return content
+        
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
--- tests.py
+++ tests.py
@@ -1,10 +1,37 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-def run_tests():
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "pkg"))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
+
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+    
+    result = get_file_content("calculator", "main.py")
+    print("Result for 'main.py' file:")
+    print(result)
+    
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for 'lorem.txt' file:")
+    print(result)
+    
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for 'calculator.py' file:")
+    print(result)
+
 
 if __name__ == "__main__":
-    run_tests()
+    test()
